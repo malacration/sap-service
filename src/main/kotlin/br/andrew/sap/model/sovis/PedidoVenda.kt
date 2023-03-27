32d351cc80eedb73e86991624e9a0ed1e463a176
Andrew Ramires May <andrewc3po@gmail.com>
2023-03-27 16:35:52 -0400

    ignorando propriedade order

--- src/main/kotlin/br/andrew/sap/model/sovis/PedidoVenda.kt
+++ src/main/kotlin/br/andrew/sap/model/sovis/PedidoVenda.kt
@@ -2,6 +2,8 @@ package br.andrew.sap.model.sovis
 
 import br.andrew.sap.model.documents.OrderSales
 import br.andrew.sap.model.documents.Product
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.util.*
 
 class PedidoVenda(
@@ -12,6 +14,7 @@ class PedidoVenda(
         val produtos : List<Produto>,
         val codVendedor : String) {
 
+    @JsonIgnore
     fun getOrder(): OrderSales {
         return OrderSales(idCliente, Date(),produtos.map { it.getProduct() },idEmpresa,"62")
     }
