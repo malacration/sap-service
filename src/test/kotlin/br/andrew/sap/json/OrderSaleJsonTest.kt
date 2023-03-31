a7be5fbf1c1cb2eb9a8c2bf727fd450bde0276b3
Andrew Ramires May <andrewc3po@gmail.com>
2023-03-31 14:58:28 -0400

    Correção testes

--- src/test/kotlin/br/andrew/sap/json/OrderSaleJsonTest.kt
+++ src/test/kotlin/br/andrew/sap/json/OrderSaleJsonTest.kt
@@ -13,7 +13,7 @@ class OrderSaleJsonTest {
     fun test(){
         val mapper = ObjectMapper().registerModule(KotlinModule())
         val produtos = listOf<Product>(Product("PRO0001","10.5","10.0"))
-        val pedido = OrderSales("CLI0001", Date(),produtos,"2",
+        val pedido = OrderSales("CLI0001", "Date()",produtos,"2",
                 "avista")
         println(mapper.writeValueAsString(pedido))
     }
