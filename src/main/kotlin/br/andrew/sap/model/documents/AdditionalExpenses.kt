f7bc2d9601b67964dd56e38e63a2b06070ddf014
Andrew Ramires May <andrewc3po@gmail.com>
2023-04-03 16:12:16 -0400

    Refatoração de despesa adicional

---
+++ src/main/kotlin/br/andrew/sap/model/documents/AdditionalExpenses.kt
@@ -0,0 +1,23 @@
+package br.andrew.sap.model.documents
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+
+@JsonNaming(PropertyNamingStrategy.UpperCamelCaseStrategy::class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+class AdditionalExpenses(val expenseCode : Int, val lineTotalSys : Double){
+
+    val distributionMethod : String = "aedm_RowTotal"
+    val lineTotal : Double = lineTotalSys
+    val LineGross : Double = lineTotalSys
+    val LineGrossSys : Double = lineTotalSys
+
+    companion object{
+        @JsonIgnore
+        fun frete(valor : Double) : AdditionalExpenses{
+            return AdditionalExpenses(1,valor)
+        }
+    }
+}
--- src/main/kotlin/br/andrew/sap/model/documents/OrderSales.kt
+++ src/main/kotlin/br/andrew/sap/model/documents/OrderSales.kt
@@ -3,7 +3,6 @@ package br.andrew.sap.model.documents
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import java.util.*
@@ -22,22 +21,5 @@ class OrderSales(CardCode: String,
 
     //TODO não achei onde fica esse propriedade
     var header : String? = null
-}
-
-
-@JsonNaming(PropertyNamingStrategy.UpperCamelCaseStrategy::class)
-@JsonIgnoreProperties(ignoreUnknown = true)
-class AdditionalExpenses(val expenseCode : Int, val lineTotalSys : Double){
-
-    val distributionMethod : String = "aedm_RowTotal"
-    val lineTotal : Double = lineTotalSys
-    val LineGross : Double = lineTotalSys
-    val LineGrossSys : Double = lineTotalSys
-    companion object{
-        @JsonIgnore
-        fun frete(valor : Double) : AdditionalExpenses{
-            return AdditionalExpenses(1,valor)
-        }
-    }
 }
 //data de entrega - ORDRdocduedate
