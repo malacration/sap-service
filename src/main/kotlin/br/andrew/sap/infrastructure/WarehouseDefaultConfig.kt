17a490ae0f84a0a94bd746b5a233b1914695eeba
Andrew Ramires May <andrewc3po@gmail.com>
2023-04-27 17:58:15 -0400

    Correção nos testes

--- src/main/kotlin/br/andrew/sap/infrastructure/WarehouseDefaultConfig.kt
+++ src/main/kotlin/br/andrew/sap/infrastructure/WarehouseDefaultConfig.kt
@@ -3,8 +3,10 @@ package br.andrew.sap.infrastructure
 import br.andrew.sap.model.WarehouseDefault
 import br.andrew.sap.services.BussinesPlaceService
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile("!test")
 class WarehouseDefaultConfig(val bussinePlace : BussinesPlaceService) {
 
     companion object{
