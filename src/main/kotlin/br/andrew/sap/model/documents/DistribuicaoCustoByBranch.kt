98ce42431101eb967c768217f46c697392de6916
Francisco Marcello <fmarcello265@gmail.com>
2023-05-25 16:56:15 -0400

    Resolvendo bug centro de custo

--- src/main/kotlin/br/andrew/sap/model/documents/DistribuicaoCustoByBranch.kt
+++ src/main/kotlin/br/andrew/sap/model/documents/DistribuicaoCustoByBranch.kt
@@ -1,6 +1,6 @@
 package br.andrew.sap.model.documents
 
-class DistribuicaoCustoByBranch(val branch: String, val centroCusto: String, val grupoEconomico: String) {
+class DistribuicaoCustoByBranch(val branch: String,val grupoEconomico: String, val centroCusto: String ) {
 
 
 }
