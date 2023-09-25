35d098267c065eebe56c9336d0860d89020507b4
Andrew Ramires May <andrewc3po@gmail.com>
2023-09-25 16:21:43 -0400

    Remove import

--- src/main/kotlin/br/andrew/sap/services/MailService.kt
+++ src/main/kotlin/br/andrew/sap/services/MailService.kt
@@ -3,13 +3,11 @@ package br.andrew.sap.services
 import br.andrew.sap.services.MailService.Companion.copyDefault
 import jakarta.mail.internet.InternetAddress
 import jakarta.mail.internet.MimeMessage
-import org.springframework.mail.MailMessage
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
 import org.thymeleaf.TemplateEngine
-import org.thymeleaf.context.Context
 
 
 @Service
