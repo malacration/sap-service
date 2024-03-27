797783c42bf3f2c38ebc84575f47baf55da365e2
Francisco Marcello <fmarcello265@gmail.com>
2024-03-27 14:39:38 -0400

    Alteração no relatório de inadiplemncia

---
+++ src/main/kotlin/br/andrew/sap/model/ParcelasAberto.kt
@@ -0,0 +1,34 @@
+package br.andrew.sap.model
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.text.SimpleDateFormat
+import java.util.Date
+
+@JsonNaming(PropertyNamingStrategy.UpperCamelCaseStrategy::class)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+class ParcelasAberto(
+    @JsonProperty("BPLName") val BPLName : String,
+    val CardCode : String,
+    val CardName : String,
+    val SlpName : String,
+    val Serial : String,
+    val InstlmntID : String,
+    val InsTotal : Double,
+    val DueDate : String,
+    val DocTotal : Double,
+    val U_StatusCobranca: String?
+){
+    fun getdueDateFormated() : String {
+        return SimpleDateFormat("dd/MM/yyy").format(SimpleDateFormat("yyyyMMdd").parse(DueDate))
+    }
+
+    fun notStatus() : String = if(U_StatusCobranca === null) "sem Status" else U_StatusCobranca
+
+
+
+}
---
+++ src/main/resources/views/titulos.sql
@@ -0,0 +1,25 @@
+SELECT
+    NS."BPLName",
+    NS."CardCode",
+    NS."CardName",
+    V."SlpCode",
+    V."SlpName",
+    V."Email" as SlpEmail,
+    NS."Serial",
+    P."InstlmntID",
+    P."InsTotal",
+    P."DueDate",
+    NS."DocTotal",
+    P."U_StatusCobranca"
+FROM OINV NS
+    INNER JOIN INV6 P ON P."DocEntry" = NS."DocEntry"
+    INNER JOIN OSLP V ON V."SlpCode" = NS."SlpCode"
+    LEFT  JOIN RCT2 CR ON CR."DocEntry" = NS."DocEntry" AND CR."DocTransId" = NS."TransId" AND CR."InstId" = P."InstlmntID"
+    LEFT  JOIN ORCT OCR ON CR."DocNum" = OCR."DocEntry"
+WHERE
+    NS."DocStatus" = 'O'
+    AND NS."BPLId" in (2,4,11,17,18,12)
+    AND P."InsTotal" <> '0'
+    AND P."DueDate" <= :data
+    AND (CR."DocNum" IS NULL AND (OCR."Canceled" = 'N' OR OCR."Canceled" IS NULL))
+    AND V."SlpCode" = :slpCode
