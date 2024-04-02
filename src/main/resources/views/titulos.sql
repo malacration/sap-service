7d3004d0b408e91dcbe5823f1d4122a1f570130b
Andrew Ramires May <andrewc3po@gmail.com>
2024-04-02 16:19:13 -0400

    COrrecao em titulos

--- src/main/resources/views/titulos.sql
+++ src/main/resources/views/titulos.sql
@@ -14,11 +14,11 @@ FROM OINV NS
     INNER JOIN INV6 P ON P."DocEntry" = NS."DocEntry"
     INNER JOIN OSLP V ON V."SlpCode" = NS."SlpCode"
     LEFT  JOIN RCT2 CR ON CR."DocEntry" = NS."DocEntry" AND CR."DocTransId" = NS."TransId" AND CR."InstId" = P."InstlmntID"
-    LEFT  JOIN ORCT OCR ON CR."DocNum" = OCR."DocEntry"
+    LEFT  JOIN ORCT OCR ON CR."DocNum" = OCR."DocEntry" AND OCR."Canceled" = 'N'
 WHERE
     NS."DocStatus" = 'O'
-    AND NS."BPLId" in (2,4,11,17)
+    AND NS."BPLId" in (2,4,11,17,18)
     AND P."InsTotal" <> '0'
     AND P."DueDate" <= :data
-    AND (CR."DocNum" IS NULL AND (OCR."Canceled" = 'N' OR OCR."Canceled" IS NULL))
+    AND CR."DocNum" IS NULL
     AND V."SlpCode" = :slpCode
