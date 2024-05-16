6837c881344389c1660a4f76905a750288b4a2a3
PedroColomb0 <pedrocolombo@csc.gruporovema.com.br>
2024-05-16 14:28:14 -0400

    sovis_api_branch2

---
+++ src/test/kotlin/br/andrew/sap/json/BPBranchAssignmentJsonTests.kt
@@ -0,0 +1,25 @@
+package br.andrew.sap.json
+
+import br.andrew.sap.model.partner.BusinessPartner
+import br.andrew.sap.model.partner.BusinessPartnerType
+import br.andrew.sap.model.partner.CpfCnpj
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.*
+
+class BusinessPartnerJsonTest {
+
+    @Test
+    fun test(){
+        val mapper = ObjectMapper().registerModule(KotlinModule())
+        val saida = mapper.writeValueAsString(BusinessPartner("windson", BusinessPartnerType.C)
+                .also { it.setCpfCnpj(CpfCnpj("01847004261")) })
+        Assertions.assertTrue(saida.contains("BPFiscalTaxIDCollection"))
+        Assertions.assertTrue(!saida.contains("BpfiscalTaxIDCollection"))
+        Assertions.assertTrue(saida.contains("BPAddresses"))
+        println(saida)
+    }
+}
