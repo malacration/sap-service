80508d3eb8020500a71ec383e4de4daa352d6657
PedroColomb0 <pedrocolombo@csc.gruporovema.com.br>
2024-06-03 17:09:35 -0400

    Criacao dos campos solicitados para o RdStation

---
+++ src/main/kotlin/br/andrew/sap/controllers/RdStationController.kt
@@ -0,0 +1,24 @@
+package br.andrew.sap.controllers
+
+import br.andrew.sap.infrastructure.odata.OData
+import br.andrew.sap.services.MotoristaContratoService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+    @RequestMapping("motorista-contrato")
+class MotoristaContratoController(val motoristaContratoService : MotoristaContratoService) {
+
+    @GetMapping()
+    fun getRegistros(): OData {
+        return motoristaContratoService.get()
+    }
+
+    @GetMapping("{id}")
+    fun getByCodParceiro(@PathVariable id : String): OData {
+        return motoristaContratoService.getById("'$id'").tryGetValue()
+    }
+}
