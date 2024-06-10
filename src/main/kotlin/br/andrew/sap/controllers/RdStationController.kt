f800b29fcc389698d77f6fe177408860cbcd4706
Andrew Ramires May <andrewc3po@gmail.com>
2024-06-10 14:33:49 -0400

    aplicando DRY ao codigo do pedro

--- src/main/kotlin/br/andrew/sap/controllers/RdStationController.kt
+++ src/main/kotlin/br/andrew/sap/controllers/RdStationController.kt
@@ -17,28 +17,28 @@ import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-    @RequestMapping("rdstation")
+@RequestMapping("rdstation")
 class RdStationController(
-    val eventsService: EventsService,
-    val bussinesPartenerService: BusinessPartnersService,
-    val slService: SalesPersonsService,
-    val ordersService: OrdersService
+    private val eventsService: EventsService,
+    private val bussinesPartenerService: BusinessPartnersService,
+    private val slService: SalesPersonsService,
+    private val ordersService: OrdersService
     ) {
 
     @GetMapping("/pedido-venda/{id}")
     fun getByCodParceiro(@PathVariable id: String): Any {
         val documento: Document = ordersService.getById(id).tryGetValue()
+        return getByCodParceiro(documento)
+    }
 
+    fun getByCodParceiro(documento: Document): Any {
         val businessPartner: BusinessPartner = bussinesPartenerService.getById("'${documento.CardCode}'").tryGetValue<BusinessPartner>()
-
         val salesPersonCode = documento.salesPersonCode
         val salesPersonCodeAll = slService.getAll<SalePerson>()
         val salePerson = salesPersonCodeAll.find { it.SalesEmployeeCode == salesPersonCode }
-
         if (salePerson == null) {
             return ResponseEntity.notFound()
         }
-
         return eventsService.conversion(documento, businessPartner, salePerson)
     }
 
