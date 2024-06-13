2c4cbf5f58bca42961aac23ab24327419660f13f
PedroColomb0 <pedrocolombo@csc.gruporovema.com.br>
2024-06-13 16:24:17 -0400

    Criacao da tela de Troca de Vendedor

---
+++ src/main/kotlin/br/andrew/sap/controllers/SalesPersonController.kt
@@ -0,0 +1,27 @@
+package br.andrew.sap.controllers
+
+import br.andrew.sap.infrastructure.odata.*
+import br.andrew.sap.services.FazendaService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("fazenda")
+class FazendaController(
+        val fazendaService: FazendaService) {
+
+
+    @GetMapping()
+    fun getFazendas() : Any{
+        return fazendaService.get()
+    }
+
+    @GetMapping("{id}")
+    fun getFazendaById(@PathVariable id : String) : OData{
+        return fazendaService.get(Filter(mutableListOf(Predicate("Code",id, Condicao.EQUAL))))
+    }
+
+
+}
