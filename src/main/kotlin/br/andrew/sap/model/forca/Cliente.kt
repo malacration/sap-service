01b849a3ac403ef4dcf6b7c1518f1332e1c273df
Andrew Ramires May <andrewc3po@gmail.com>
2024-06-28 16:03:58 -0400

    Melhoria no cadastro de cliente e aceitar atualizacao de clitens

--- src/main/kotlin/br/andrew/sap/model/forca/Cliente.kt
+++ src/main/kotlin/br/andrew/sap/model/forca/Cliente.kt
@@ -1,19 +1,14 @@
 package br.andrew.sap.model.forca
 
-import br.andrew.sap.model.partner.BusinessPartner
-import br.andrew.sap.model.partner.BusinessPartnerType
-import br.andrew.sap.model.partner.BPBranchAssignment
-import br.andrew.sap.model.partner.CpfCnpj
+import br.andrew.sap.model.partner.*
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-class Cliente(
-        val nome : String,
-        val cpfCnpj : String,
-        val endereco : Endereco,
-        val idCliente : String){
+class Cliente(val nome : String, val cpfCnpj : String){
 
+    var endereco : Endereco? = null
+    var idCliente : String? = null
     var telefone : String? = null
     val email : String? = null
     val idVendedor : String? = null
@@ -28,14 +23,36 @@ class Cliente(
     fun getBusinessPartner() : BusinessPartner {
         return BusinessPartner(nome, BusinessPartnerType.C).also {
             it.setCpfCnpj(CpfCnpj(cpfCnpj),ierg)
-            it.setAddresse(endereco.getAddresse(localidade))
+            if(endereco != null)
+                it.setAddresse(endereco!!.getAddresse(localidade))
             it.u_id_forca = idCliente
             it.phone1 = telefone
             it.emailAddress = email
             it.salesPersonCode = idVendedor?.toInt() ?: -1
             it.freeText = obscadastral
             it.U_fazer_fluxo_prazo = "1"
-            it.U_Rov_Data_Nascimento = dtnasc
+            it.U_Rov_Data_Nascimento = if(dtnasc?.trim().isNullOrEmpty()) null else dtnasc
         }
+    }
+
+
+    @JsonIgnore
+    @JsonIgnoreProperties
+    fun getUpdateBusinessPartner(cardCode : String) : BusinessPartner {
+        return BusinessPartner(nome, BusinessPartnerType.C).also {
+            it.cardCode = cardCode
+            it.series = null
+            it.U_fazer_fluxo_prazo = null
+            it.cardType = null
+            it.u_id_forca = idCliente
+            it.phone1 = telefone
+            it.emailAddress = email
+            it.freeText = obscadastral
+            it.U_Rov_Data_Nascimento = if(dtnasc?.trim().isNullOrEmpty()) null else dtnasc
+        }
+    }
+
+    fun getAddresForUpdate(): Address? {
+        return endereco?.getAddresse(localidade)
     }
 }
