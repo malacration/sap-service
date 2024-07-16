c6e10236ca19d7be4b9ca27d70eee5bd4e8a45c5
Andrew Ramires May <andrewc3po@gmail.com>
2024-07-16 16:00:12 -0400

    Correcao em serializar codigo de imposto e provisonamento de versao

---
+++ src/main/kotlin/br/andrew/sap/model/exceptions/CreditException.kt
@@ -0,0 +1,23 @@
+package br.andrew.sap.model.exceptions
+
+import br.andrew.sap.model.SapError
+import br.andrew.sap.model.documents.OrderSales
+import br.andrew.sap.model.documents.base.Document
+
+class CreditException(error: SapError, val location : String?, cause: Throwable? = null) : SapGenericException(error, cause, "Detecção de erro relacionado a Credito do PN") {
+    //TODO é possivel fazer a chamada a draft para verificar os valores
+
+    val idLocation : String = "(\\d+)(?=\\)$)".toRegex().find(location?:"")?.value ?: ""
+
+    fun getDocumentFake(order: Document? = null) : Document{
+        return order?.also {
+            it.docEntry = idLocation.toInt()
+            it.docNum = idLocation
+        }
+            ?: OrderSales("CardCode", "", listOf(),"")
+                .also {
+                    it.docEntry = idLocation.toInt()
+                    it.docNum = idLocation
+                }
+    }
+}
