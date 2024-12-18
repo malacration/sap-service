b93f0cf3ef31180de3cb1970a2e24f558bf98df9
Pedr0Colombo <pedro.colomboprof1@gmail.com>
2024-12-18 16:17:19 -0400

    gerar pdf

---
+++ src/main/kotlin/br/andrew/sap/infrastructure/configurations/JacksonTimeZoneConfig.kt
@@ -0,0 +1,18 @@
+package br.andrew.sap.infrastructure.configurations
+
+import br.andrew.sap.model.sap.SapUser
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+import org.springframework.context.annotation.Profile
+
+
+@Profile("test || aws")
+@Configuration
+class MockConfig {
+
+    @Bean(name = arrayOf("user.current"))
+    fun getCurrentUser() : SapUser {
+        return SapUser(666)
+    }
+}
