bc2d29f640b56c683a0b32f37e8841b9f77d5922
Pedr0Colombo <pedro.colomboprof1@gmail.com>
2024-12-23 16:56:08 -0400

    ajuste dos testes

--- src/test/kotlin/br/andrew/sap/json/InstallmentJsonTests.kt
+++ src/test/kotlin/br/andrew/sap/json/InstallmentJsonTests.kt
@@ -1,8 +1,9 @@
 package br.andrew.sap.json
 
-import br.andrew.sap.model.forca.Cliente
 import br.andrew.sap.model.sap.documents.base.Installment
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonTypeRef
 import org.junit.jupiter.api.Assertions
@@ -10,13 +11,15 @@ import org.junit.jupiter.api.Test
 
 class InstallmentJsonTests {
 
-
     @Test
-    fun teste(){
+    fun teste() {
         val json = "{\"DueDate\":\"2025-01-15\",\"Total\":1200}"
-        val mapper = ObjectMapper().registerModule(KotlinModule())
+        val mapper = ObjectMapper()
+            .registerModule(KotlinModule())
+            .registerModule(JavaTimeModule())
+            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         val obj = mapper.readValue(json, jacksonTypeRef<Installment>())
         println(obj)
-        Assertions.assertEquals("2025-01-15",obj.dueDate)
+        Assertions.assertEquals("2025-01-15", obj.dueDate.toString())
     }
 }
