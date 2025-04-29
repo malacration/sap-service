85bf60bbf13c9844fb1de5a1f1b17bd26ec35636
Pedr0Colombo <pedro.colomboprof1@gmail.com>
2025-04-29 14:52:20 -0400

    adicionando filial e buscar localidade

--- src/main/kotlin/br/andrew/sap/controllers/BusinessPartnersController.kt
+++ src/main/kotlin/br/andrew/sap/controllers/BusinessPartnersController.kt
@@ -168,4 +168,8 @@ class BusinessPartnersController(
         return service.getByCpfCnpj(cpfCnpj,tipo).getContactOpaque()
     }
 
+    @GetMapping("search-localidade")
+    fun search(): List<BusinessPartner> {
+        return service.searchLocalidade()
+    }
 }
