9c1f7b08375e3aa8d92667eb800c899dacd5f9ee
Francisco Marcello <fmarcello265@gmail.com>
2025-08-08 09:48:39 -0400

    Ajuste no relatorio de cobrança

--- src/main/kotlin/br/andrew/sap/model/ParcelasAberto.kt
+++ src/main/kotlin/br/andrew/sap/model/ParcelasAberto.kt
@@ -1,10 +1,12 @@
 package br.andrew.sap.model
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import java.math.BigDecimal
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -18,11 +20,17 @@ class ParcelasAberto(
     val SlpName : String,
     val Serial : String,
     val InstlmntID : String,
-    val InsTotal : Double,
+    val InsTotal : BigDecimal,
     val DueDate : String,
     val DocTotal : Double,
-    val U_StatusCobranca: String?
+    val U_StatusCobranca: String?,
+    val U_AgenteCobrador: String?,
+    val PaidToDate : BigDecimal
 ){
+    @get:JsonIgnore
+    val saldo: BigDecimal
+        get() = InsTotal.subtract(PaidToDate)
+
     fun getdueDateFormated() : String {
         return SimpleDateFormat("dd/MM/yyy").format(SimpleDateFormat("yyyyMMdd").parse(DueDate))
     }
